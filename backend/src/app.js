cf4f8dec86c056bb4d785f3339ed53bd82472e95
patrick <theofficesaver@gmail.com>
2026-03-28 19:09:02 +0800

    hong

--- backend/src/app.js
+++ backend/src/app.js
@@ -9,7 +9,7 @@ app.use(bodyParser.json())
 
 //define GET routes
 app.get('/', (req, res) => {
-  res.send('Hello from express udpate 2015678 ')
+  res.send('Hello from express udpate 20156789 ')
 })
 
 postsRoutes(app)
