7d0129e1d960636af19645c7ffeb97632b99998e
patrick <theofficesaver@gmail.com>
2026-04-29 12:31:30 +0800

    fix authfixture

--- tests/fixtures/AuthFixture.js
+++ tests/fixtures/AuthFixture.js
@@ -3,25 +3,7 @@ export class AuthFixture {
     this.page = page
   }
   async signUpAndLogIn() {
-    await page.getByRole('button', { name: '重新加载' }).click();
-    await page.getByRole('button', { name: '重新加载' }).click();
-    await page.locator('html').click();
-    await page.getByRole('textbox', { name: 'Username:' }).click();
-    await page.getByRole('textbox', { name: 'Username:' }).fill('test');
-    await page.getByRole('textbox', { name: 'Username:' }).press('Tab');
-    await page.getByRole('textbox', { name: 'Password:' }).fill('test');
-    await page.getByRole('textbox', { name: 'Password:' }).press('Enter');
-    page.once('dialog', dialog => {
-      console.log(`Dialog message: ${dialog.message()}`);
-      dialog.dismiss().catch(() => { });
-    });
-    await page.getByRole('button', { name: 'Sign Up' }).click();
-    page.once('dialog', dialog => {
-      console.log(`Dialog message: ${dialog.message()}`);
-      dialog.dismiss().catch(() => { });
-    });
-    await page.getByRole('button', { name: 'Sign Up' }).click();
-    await page.getByRole('textbox', { name: 'Password:' }).click();
+
     const testUser = 'test' + Date.now()
     await this.page.goto('/signup')
     await this.page.getByLabel('Username:').fill(testUser)
